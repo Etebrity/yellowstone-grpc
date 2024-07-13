84ab40e763ef95e9c0ae557a786292f5ad1453bb
Etebrity <97344559+Etebrity@users.noreply.github.com>
2024-07-13 23:31:54 +0300

    fix

--- yellowstone-grpc-proto/build.rs
+++ yellowstone-grpc-proto/build.rs
@@ -1,5 +1,9 @@
 fn main() -> anyhow::Result<()> {
-    std::env::set_var("PROTOC", protobuf_src::protoc());
+    const PROTOC_ENVAR: &str = "PROTOC";
+    if std::env::var(PROTOC_ENVAR).is_err() {
+        #[cfg(not(windows))]
+        std::env::set_var(PROTOC_ENVAR, protobuf_src::protoc());
+    }
     tonic_build::compile_protos("proto/geyser.proto")?;
     Ok(())
 }
